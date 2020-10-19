b15a6df2b0c35d57157a5326336bdb8b52cf4eaf
樊建政 <fanjianzheng@synjones.net>
2020-10-19 17:15:25 +0800

    创建工程

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by 樊建政 on 2020/10/19.
+//  Copyright © 2020 com.synjonesapp.test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DYZB/DYZBTests/DYZBTests.swift
@@ -0,0 +1,34 @@
+//
+//  DYZBTests.swift
+//  DYZBTests
+//
+//  Created by 樊建政 on 2020/10/19.
+//  Copyright © 2020 com.synjonesapp.test. All rights reserved.
+//
+
+import XCTest
+@testable import DYZB
+
+class DYZBTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
